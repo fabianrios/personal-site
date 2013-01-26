2d6af7947b4b511051c42800d4f34d851abaf9f6
fabianrios <hola@fabianrios.co>
2013-01-26 00:33:53 -0500

    Validation!

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ActiveRecord::Base
   attr_accessible :commercial, :description, :fecha, :image, :label, :link_url, :name
+  validates :name, :description, :image, :link_url, :presence => true
+  validates :name, :uniqueness => true
+  validates :link_url, :uniqueness => true
+  validates :image, :format => {
+  :with => %r{\.(gif|jpg|png)$}i,
+  :message => 'debe ser una URL con una imagen GIF, JPG o PNG.'
+  }
 end
--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -3,6 +3,15 @@ require 'test_helper'
 class ProjectsControllerTest < ActionController::TestCase
   setup do
     @project = projects(:one)
+    @update = {
+    :commercial => 'lorem ipsum sit a met corps sa ers',
+    :description => 'Wibbles are fun!' ,
+    :fecha => '2013-01-26 04:53:00 UTC',
+    :image => 'lorem.jpg' ,
+    :label => 'Aplicacion web',
+    :link_url => 'fabianrios.co/',
+    :name => 'Lorem Ipsum' 
+    }
   end
 
   test "should get index" do
@@ -18,7 +27,8 @@ class ProjectsControllerTest < ActionController::TestCase
 
   test "should create project" do
     assert_difference('Project.count') do
-      post :create, project: { commercial: @project.commercial, description: @project.description, fecha: @project.fecha, image: @project.image, label: @project.label, link_url: @project.link_url, name: @project.name }
+      # post :create, project: { commercial: @project.commercial, description: @project.description, fecha: @project.fecha, image: @project.image, label: @project.label, link_url: @project.link_url, name: @project.name }
+      post :create, :project => @update
     end
 
     assert_redirected_to project_path(assigns(:project))
@@ -35,7 +45,8 @@ class ProjectsControllerTest < ActionController::TestCase
   end
 
   test "should update project" do
-    put :update, id: @project, project: { commercial: @project.commercial, description: @project.description, fecha: @project.fecha, image: @project.image, label: @project.label, link_url: @project.link_url, name: @project.name }
+    # put :update, id: @project, project: { commercial: @project.commercial, description: @project.description, fecha: @project.fecha, image: @project.image, label: @project.label, link_url: @project.link_url, name: @project.name }
+    put :update, :id => @project.to_param, :project => @update
     assert_redirected_to project_path(assigns(:project))
   end
 
